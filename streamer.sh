4a6a32fe749a51414383e9f4f0abbe25bdc2d92a
Jesse <mr.bossman075@gmail.com>
2021-08-16 09:10:47 -0400

    real rick

--- streamer.sh
+++ streamer.sh
@@ -3,4 +3,4 @@ export COLORTERM=truecolor
 export TERM=xterm-256color
 export COLUMNS=$2
 export LINES=$1
-youtube-dl -f webm https://www.youtube.com/watch?v=HUgMWJKn2YY -o -  | ffmpeg -re -i pipe: -vcodec rawvideo -r 20 -an -color full16  -antialias  prefilter -algorithm  fstein -pix_fmt rgb24 -window_size $2x$1 -driver ncurses -f caca - 2> /dev/null
+youtube-dl -f webm https://www.youtube.com/watch?v=dQw4w9WgXcQ -o -  | ffmpeg -re -i pipe: -vcodec rawvideo -r 20 -an -color full16  -antialias  prefilter -algorithm  fstein -pix_fmt rgb24 -window_size $2x$1 -driver ncurses -f caca - 2> /dev/null
