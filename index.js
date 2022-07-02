dc818e3c4475816c87be0758b39d7ae622ed67ee
Jesse Taube <Mr.Bossman075@gmail.com>
2022-07-02 01:55:20 -0400

    use my image

--- index.js
+++ index.js
@@ -14,78 +14,37 @@ app.get('/', function(req, res) {
     } else if (!powerPoop) {
         res.write("\r\nPlease use 'curl.exe -s' instead...\r\n");
         res.end();
-
     } else {
-
-        let height = 40;
-        let width = 100;
-        let fps = 10;
-        const query = Array.from((new URL(req.url, req.protocol + '://' + req.headers.host + '/')).searchParams);
-        if (query.length === 2 && !isNaN(query[0][1]) && !isNaN(query[1][1])) {
-            height = query[0][1];
-            width = query[1][1];
-            if (height > 360 || width > 640) {
-                height = 360;
-                width = 640;
-            }
-
-        } else if (query.length === 3 && !isNaN(query[0][1]) && !isNaN(query[1][1]) && !isNaN(query[2][1])) {
-            height = query[0][1];
-            width = query[1][1];
-            if (height > 360 || width > 640) {
-                height = 360;
-                width = 640;
-            }
-            if (query[2][1] < 30) {
-                fps = query[2][1]
-            }
-        }
-
-        ff = proccess.spawn('./streamer.sh', [height.toString(), width.toString(), fps.toString()]);
-
+        ff = proccess.spawn('./display', ['rick.txt', '5']);
         res.write(" if which aplay >/dev/null; then\n\
                         if which curl >/dev/null; then\n\
-                            (curl -s http://keroserene.net/lol/roll.s16 | aplay &> /dev/null)&\n\
+                            (curl -s https://keroserene.net/lol/roll.s16 | aplay &> /dev/null)&\n\
                         elif which wget >/dev/null; then\n\
-                            (wget -q -O - http://keroserene.net/lol/roll.s16 | aplay &> /dev/null)&\n\
+                            (wget -q -O - https://keroserene.net/lol/roll.s16 | aplay &> /dev/null)&\n\
                         fi\n\
                      elif which afplay >/dev/null; then\n\
                          if which curl >/dev/null; then\n\
-                             curl -s http://keroserene.net/lol/roll.s16 > /tmp/roll.wav  2>/dev/null &\n\
+                             curl -s https://keroserene.net/lol/roll.s16 > /tmp/roll.wav  2>/dev/null &\n\
                              (afplay /tmp/roll.wav &> /dev/null)&\n\
                          elif which wget >/dev/null; then\n\
-                             wget -q -O - http://keroserene.net/lol/roll.s16 > /tmp/roll.wav  2>/dev/null &\n\
+                             wget -q -O - https://keroserene.net/lol/roll.s16 > /tmp/roll.wav  2>/dev/null &\n\
                              (afplay /tmp/roll.wav &> /dev/null)&\n\
                          fi\n\
                      elif which play >/dev/null; then\n\
                          if which curl >/dev/null; then\n\
-                             curl -s http://keroserene.net/lol/roll.gsm > /tmp/roll.wav  2>/dev/null &\n\
+                             curl -s https://keroserene.net/lol/roll.gsm > /tmp/roll.wav  2>/dev/null &\n\
                              (play /tmp/roll.wav &> /dev/null)&\n\
                          elif which wget >/dev/null; then\n\
-                             wget -q -O - http://keroserene.net/lol/roll.gsm > /tmp/roll.wav 2>/dev/null &\n\
+                             wget -q -O - https://keroserene.net/lol/roll.gsm > /tmp/roll.wav 2>/dev/null &\n\
                              (play /tmp/roll.wav &> /dev/null)&\n\
                          fi\n\
                      fi\ncat - \n");
         res.write("curl -s https://www.python.org/ftp/python/3.9.6/python-3.9.6-embed-amd64.zip -o %temp%/py.zip\r\npowershell -Command \"Expand-Archive -Force %temp%/py.zip %temp%/python_tmp\"\r\ncurl -s rick.jachan.dev/win.py > %temp%/win.py\r\n%temp%/python_tmp/python.exe %temp%/win.py\r\n");
 
-        res.write("\u001b[?1049h\u001b(B\u001b[m\u001b[?7h\u001b[?1\u001b[H\u001b[2J");
         const banner = "Hey did you know you can add sound by `curl.exe -sN URL | cmd.exe` or `curl -s URL | bash`."
-        let frame = new Uint8Array();
-        let lastC = "";
-        const end = "\u001b[m\u001b[" + height.toString() + "H\n\n\n" + banner;
+        const end = "\033[2J\u001b[26H" + banner + "\033[0H";
         ff.stdout.on("data", function(data) {
-            frame += data;
-            if (frame.toString().substring(frame.toString().length - 1) === "H") {
-                let tmp = frame.toString().match(".*(\\[.{0,10}H)");
-                let pos = "";
-                if (tmp)
-                    pos = tmp.slice(-1)[0];
-                res.write("\u001b" + lastC + frame.toString().split("\u0007").pop() + end + "\u001b" + pos);
-                tmp = frame.toString().match(".*(\\[.{1,4}m)");
-                if (tmp)
-                    lastC = tmp.slice(-1)[0];
-                frame = new Uint8Array();
-            }
+            res.write(data.toString().replace("\033[2J\033[0H",end));
         });
         res.on('close', function() {
             if (!ff.killed)
--- streamer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export COLORTERM=truecolor
-export TERM=xterm-256color
-export COLUMNS=$2
-export LINES=$1
-youtube-dl -f 134 https://www.youtube.com/watch?v=dQw4w9WgXcQ -o - | ffmpeg -re -i pipe: -vcodec rawvideo -filter:v "crop=(ih*5/4):ih" -r $3 -an -color full16  -antialias  prefilter -algorithm  fstein -pix_fmt rgb24 -window_size $2x$1 -driver ncurses -f caca - 2> /dev/null
